41a717236d7a243373c6772c51f9c5eb8177ecf1
James <ismay.james@gmail.com>
2024-02-28 14:41:28 +0000

    test

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module bs2202.objectorientedprogramming {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens bs2202.objectorientedprogramming to javafx.fxml;
+    exports bs2202.objectorientedprogramming;
+}
